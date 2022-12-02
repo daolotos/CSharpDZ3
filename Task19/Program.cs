82eda62722788fc5b0f4efe382477bee402a17f5
daolotos <117892786+daolotos@users.noreply.github.com>
2022-12-02 12:11:34 +0300

    Написала определение полиндрома

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,9 +1,26 @@
 ﻿using System;
 
 Console.WriteLine("Введите пятизначное число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-int b = a / 10;
-int c = b % 10;
+int number = Convert.ToInt32(Console.ReadLine());
+int left = 10000;
+int right = 1;
+bool isPolyndrom = true;
 
-Console.WriteLine(c);
+for (int i = 0; i < 2; i ++)
+{
+    int leftDigit = (number / left) % 10;
+    int rightDigit = (number / right) % 10;
+    if (leftDigit != rightDigit)
+    {
+        isPolyndrom = false;
+        break;
+    }
+    leftDigit /= 10;
+    rightDigit *= 10;
+}
+
+if (isPolyndrom)
+    Console.WriteLine("Это полиндром");
+else
+    Console.WriteLine("Это не полиндром");
 
